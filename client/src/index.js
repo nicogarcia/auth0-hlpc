7fe2cf6d23b817704e29b22496d8e96190da2065
nicogarcia <nicoalbo90@gmail.com>
2017-09-12 22:19:12 -0300

    Mobx store created and added to App

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "codemirror/lib/codemirror.css";
+import {observable} from "mobx";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = observable({
+    editor: {collapsed: true}
+});
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
