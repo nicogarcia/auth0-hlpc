3012e003a18fbb67d653d256ef9e9c15e181abd0
nicogarcia <nicoalbo90@gmail.com>
2017-09-16 11:33:10 -0300

    Improved title layout

--- client/src/ui/preview/header/PreviewHeader.js
+++ client/src/ui/preview/header/PreviewHeader.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./PreviewHeader.css";
-import {Button} from "@auth0/styleguide-react-components/lib/index";
+import {Button, Col} from "@auth0/styleguide-react-components/lib/index";
 import {observer} from "mobx-react";
 import PropTypes from "prop-types";
 
 const PreviewHeader = ({editorCollapsed, loginPageUrl, onToggleEditor}) => {
     return (
         <div>
-            <h4 className="left">
+            <h4 className="pull-left">
                 Current Login Page
             </h4>
-            <Button onClick={onToggleEditor}>
-                {editorCollapsed ? 'Open Editor' : 'Close editor'}
-            </Button>
-            <Button href={loginPageUrl} target="_blank">Open live version</Button>
+            <div className="pull-right">
+                <Button onClick={onToggleEditor}>
+                    {editorCollapsed ? 'Open Editor' : 'Close editor'}
+                </Button>
+                <Button href={loginPageUrl} target="_blank">Open live version</Button>
+            </div>
         </div>
     );
 };
